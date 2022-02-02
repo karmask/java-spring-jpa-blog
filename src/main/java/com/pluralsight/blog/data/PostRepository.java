3944f5f8101f1e10738c686239a2165eb83cb301
Bilvi <massimo.bilvi@accenture.com>
2022-02-02 16:11:46 +0100

    Completed module 2

--- src/main/java/com/pluralsight/blog/data/PostRepository.java
+++ src/main/java/com/pluralsight/blog/data/PostRepository.java
@@ -1,7 +1,14 @@
 package com.pluralsight.blog.data;
 
+import java.util.List;
+
+import com.pluralsight.blog.model.Category;
 import com.pluralsight.blog.model.Post;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PostRepository extends JpaRepository<Post, Long> {}
+public interface PostRepository extends JpaRepository<Post, Long> {
+
+        List<Post> findByCategory(Category category);
+}
+
